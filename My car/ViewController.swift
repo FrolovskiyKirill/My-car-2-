866b9aeda30fdce331aff8b66e2af0eeb502e976
Kirill <frolkv@gmail.com>
2023-04-05 20:38:00 +0300

    Initial Commit

---
+++ My car/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My car
+//
+//  Created by Kirill Frolovskiy on 05.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .lightGray
+  }
+
+
+}
+

2a09a6f0110f412315399b8882cded488b3d3be8
Kirill <frolkv@gmail.com>
2023-04-06 19:44:46 +0300

    Some updates

---
+++ My car/TaskListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  My car
+//
+//  Created by Kirill Frolovskiy on 05.04.2023.
+//
+
+import UIKit
+
+class TaskListViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    setupNavigationBar()
+  }
+
+
+  /// ```
+  /// Navigation bar setup
+  /// ```
+  private func setupNavigationBar() {
+    title = "Expenses List"
+    navigationController?.navigationBar.prefersLargeTitles = true
+
+    let navBarApperance = UINavigationBarAppearance()
+    navBarApperance.configureWithOpaqueBackground()
+    navBarApperance.backgroundColor = UIColor(red: 228/255, green: 228/255, blue: 235/255, alpha: 1.0)
+
+    navBarApperance.titleTextAttributes = [.foregroundColor: UIColor.white]
+    navBarApperance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+
+    navigationController?.navigationBar.standardAppearance = navBarApperance
+    navigationController?.navigationBar.scrollEdgeAppearance = navBarApperance
+
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewTask))
+
+    navigationController?.navigationBar.tintColor = .white
+  }
+
+  @objc private func addNewTask() {
+
+  }
+}
+
